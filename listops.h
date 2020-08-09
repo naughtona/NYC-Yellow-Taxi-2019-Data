3a50a7b48566bfaf8b473bb19811a7ac7a4d1c1c
Andrew Naughton <59426916+naughtona@users.noreply.github.com>
2020-08-09 14:31:41 +1000

    add ownership and include guards #ifndef

--- listops.h
+++ listops.h
@@ -1,5 +1,15 @@
+// Andrew Naughton 9/8/2020
+
+#ifndef LISTOPS_H
+#define LISTOPS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
 /* This source file is largely copied from code written by Alistair Moffat, 
- * distributed by Jianzhong Qi in FOA last semester.
+ * distributed by Jianzhong Qi in FOA 2019.
  */
  
 struct llNode {
@@ -20,3 +30,5 @@ struct llList *insertAtFoot(struct llList *list, char **values);
 
 /* Frees the list, given the structure of the linked list nodes as above. */
 void freeList(struct llList *list);
+
+#endif
